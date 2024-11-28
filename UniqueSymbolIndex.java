2cd1a9e48fa3f3fe0a145c4c7a95e48e32d75b3e
Oleg Yakovlev <oyakovinbox@gmail.com>
2024-11-28 20:15:15 +0100

    https://leetcode.com/problems/first-unique-character-in-a-string/

--- UniqueSymbolIndex.java
+++ UniqueSymbolIndex.java
@@ -4,6 +4,7 @@ import java.util.Set;
 
 public class UniqueSymbolIndex {
 
+    // My first solution
     public static int firstUniqChar(String s) {
         HashMap<Character, Integer> charFirstOccurrence = new HashMap<>();
         Set<Character> repeatingCharacters = new HashSet<>();
@@ -24,6 +25,26 @@ public class UniqueSymbolIndex {
             if(charFirstOccurrence.get(c) < earliestOccurrence) earliestOccurrence = charFirstOccurrence.get(c);
         }
         return earliestOccurrence;
+    }
+
+    public int firstUniqChar_most_efficient(String s) {
+        // Premade array for all chars in english
+        int[] ar = new int[26];
+        char[] c = s.toCharArray();
+
+        // Number of occurrences for each element
+        for (int i = 0; i < c.length; i++) {
+            // Encoding index of the char as its code relative to 'a's in ASCII
+            ar[((int) (c[i])) - ((int) ('a'))]++;
+        }
+
+        // First element in the string that has one occurrence
+        for (int i = 0; i < c.length; i++) {
+            if (ar[((int) (c[i])) - ((int) ('a'))] == 1) {
+                return i;
+            }
+        }
+        return -1;
     }
 
     public static void main(String[] args) {
