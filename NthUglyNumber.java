84f90df872cf6434a1f8b62444686dca0b5627fe
Oleg Yakovlev <oyakovinbox@gmail.com>
2024-11-29 23:46:22 +0100

    https://leetcode.com/problems/flatten-nested-list-iterator/

---
+++ NthUglyNumber.java
@@ -0,0 +1,27 @@
+import java.util.*;
+
+public class NthUglyNumber {
+
+    // https://leetcode.com/problems/super-ugly-number/solutions/277313/my-view-of-this-question-hope-it-can-help-you-understand/
+    public int nthSuperUglyNumber(int n, int[] primes) {
+        PriorityQueue<int[]> queue=new PriorityQueue<>((a,b)->(a[0]-b[0]));
+        for (int i=0;i<primes.length;i++)
+            queue.offer(new int[]{primes[i], primes[i], 0});
+
+        int[] nums=new int[n+1];
+        nums[0]=1;
+
+        int i=1;
+        while (i<n){
+            int[] entry=queue.poll();
+            int num=entry[0], prime=entry[1], index=entry[2];
+            // remove duplicate
+            if (num!=nums[i-1]){
+                nums[i]=num;
+                i++;
+            }
+            queue.offer(new int[]{prime*nums[index+1], prime, index+1});
+        }
+        return nums[n-1];
+    }
+}
